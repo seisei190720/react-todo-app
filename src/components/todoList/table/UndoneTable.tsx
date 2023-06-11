80174364daf89ee105a7943b5842e93230840378
Ezawa Seiya <ezawa_s@works-hi.co.jp>
2023-06-11 20:05:44 +0900

    mod

--- src/components/todoList/table/UndoneTable.tsx
+++ src/components/todoList/table/UndoneTable.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import LowPriorityIcon from "@mui/icons-material/LowPriority";
 import VerticalAlignTopSharpIcon from "@mui/icons-material/VerticalAlignTopSharp";
